f732620775d9844935164976a380848aa7a5a7af
Mike Tamis <tamis.mike@gmail.com>
2019-04-09 08:56:28 +1200

    Added spark themes

--- build.js
+++ build.js
@@ -6,30 +6,35 @@ const config = JSON.parse(fs.readFileSync('./config.json'));
 
 console.log('Build started...');
 
-const StyleDictionary = require('style-dictionary').extend(config);
-StyleDictionary.buildAllPlatforms();
+require('./generateThemes').then(() => {
 
-const varitationTypes = fs.readdirSync('./variations');
 
-const base = StyleDictionary.exportPlatform('web/json');
-const output = {
-    base,
-}
-
-varitationTypes.map(function (variationType) {
-    console.log('\n==============================================');
-    output[variationType] = {};
-    fs.readdirSync(`./variations/${variationType}`).map(function (variation) {
+    const StyleDictionary = require('style-dictionary').extend(config);
+    StyleDictionary.buildAllPlatforms();
+    
+    const varitationTypes = fs.readdirSync('./variations');
+    
+    const base = StyleDictionary.exportPlatform('web/json');
+    const output = {
+        base,
+    }
+    
+    varitationTypes.map(function (variationType) {
         console.log('\n==============================================');
-        console.log('Collision from here are normal');
-        const myConfig = Object.assign({}, config);
-        myConfig.source.push(`variations/${variationType}/${variation}/**/*.json`)
-        const StyleDictionaryVariation = require('style-dictionary').extend(config);
-        output[variationType][variation] = deepDiff(base, StyleDictionaryVariation.exportPlatform('web/json'), true);
-        
-    });
-})
+        output[variationType] = {};
+        fs.readdirSync(`./variations/${variationType}`).map(function (variation) {
+            console.log('\n==============================================');
+            console.log('Collision from here are normal');
+            const myConfig = Object.assign({}, config);
+            myConfig.source.push(`variations/${variationType}/${variation}/**/*.json`)
+            const StyleDictionaryVariation = require('style-dictionary').extend(config);
+            output[variationType][variation] = deepDiff(base, StyleDictionaryVariation.exportPlatform('web/json'), true);
+            
+        });
+    })
+    
+    console.log('\n==============================================');
+    console.log('Writing tokens with variations');
+    fs.writeFileSync('./build/web/tokensWithVariations.json', JSON.stringify(output));
 
-console.log('\n==============================================');
-console.log('Writing tokens with variations');
-fs.writeFileSync('./build/web/tokensWithVariations.json', JSON.stringify(output));
+});
